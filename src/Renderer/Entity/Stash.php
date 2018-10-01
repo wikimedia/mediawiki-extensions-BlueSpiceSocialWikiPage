64dcf31119cec0ec8645e41f1df88a02cc88bfaa
pwirth <wirth@hallowelt.biz>
2018-10-01 11:06:12 +0200

    Remove content rendering from entity renderer

--- src/Renderer/Entity/Stash.php
+++ src/Renderer/Entity/Stash.php
@@ -2,4 +2,9 @@
 
 namespace BlueSpice\Social\WikiPage\Renderer\Entity;
 
-class Stash extends \BlueSpice\Social\Renderer\Entity\Text{}
+class Stash extends \BlueSpice\Social\Renderer\Entity\Text {
+
+	protected function render_content( $val ) {
+		return '';
+	}
+}
