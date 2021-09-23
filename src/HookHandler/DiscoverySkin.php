53e484d2c8bd99c031aee78d8415761ab3113243
pwirth <wirth@hallowelt.biz>
2021-09-23 12:36:24 +0200

    Add stash component to after content skin slot for discovery skin
    ERM:25775
    [REL1_35-4.0.x]
    
    Change-Id: Ida2739b7ac73aacb0873ffcf8f4b263453a5a015

---
+++ src/HookHandler/DiscoverySkin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BlueSpice\Social\WikiPage\HookHandler;
+
+use BlueSpice\Social\WikiPage\Component\AfterContent;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+
+class DiscoverySkin implements MWStakeCommonUIRegisterSkinSlotComponents {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		$registry->register(
+			'DataAfterContent',
+			[
+				'social-stash' => [
+					'factory' => static function () {
+						return new AfterContent();
+					}
+				]
+			]
+		);
+	}
+}
